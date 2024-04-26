3ab53b10ae291ae3ed9a843eb5ba85b24f0dc2e5
tamakeri <cagdas20001@gmail.com>
2024-04-26 14:05:15 +0300

    improwed  array system
     not complete

---
+++ a.py
@@ -0,0 +1,4 @@
+a="<item"
+i=0
+print(a[i])
+print(a[i:i+5])
--- main.py
+++ main.py
@@ -10,20 +10,41 @@ aranan_dosya = [x for x in li2 if x not in s]
 import time
 f = open("thisss i", "r")
 
-arr=[]
+s=""
 for i in f:
-    print(i)
+    s=s+i
+flag=False
+temp=""
+real_arr=[]
 
 
 
-    #literaly  line arıyor daha iyi bir wire  aram a yöntemi bul
-    time.sleep(5)
-    if(i.find("wire")>0):
+for i in range(len (s)):
 
-
+    if (flag):
+        temp=temp+s[i]
         
-        arr.append(i)
-        arr.append("|||")
-    
 
-print(arr[1])
+
+    if(s[i]=="<"):
+        if(s[i:i+6]=="<Item "): 
+            flag=True
+            temp=temp+s[i]
+
+        if(s[i:i+7]=="</Item>"): 
+            flag=False
+            temp=temp+s[i]
+            real_arr.append(temp)
+            temp=""
+            
+
+print(real_arr)
+
+
+
+f = open("demofile2.txt", "a")
+for i in range(len (real_arr)):
+    f.write(real_arr[i])
+f.close()
+
+#open and read the file after the appending:
